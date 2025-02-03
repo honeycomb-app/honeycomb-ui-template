f2ada2141acefb9ec8faacc9002e15e276ccc396
Mijura <miodragvilotijevic@gmail.com>
2025-02-03 15:47:31 +0100

    @jigjoy-io/ui instalation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function App() {
-	return <h1 className="bg-surface-1 text-heading p-4">JigJoy UI library</h1>
+	return <h1 className="bg-surface1 text-heading p-4 font-bold text-3xl">JigJoy UI library</h1>
 }
